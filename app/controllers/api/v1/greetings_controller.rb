3daa8732cac6f3ceaf62398f77b42f16b65b43fa
SiansandaTamara <tamarasiansanda18@gmail.com>
2022-10-22 23:05:38 +0200

    create app

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
-    def index
-        greetings = Greeting.all
-        render json: greetings[rand(greetings.length - 1)]
-      end
+  def index
+    greetings = Greeting.all
+    render json: greetings[rand(greetings.length - 1)]
+  end
 end
